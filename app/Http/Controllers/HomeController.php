adee89cb235479c1d4d31d25474fb9780f3973fe
Lambertn33 <lambertgithub@gmail.com>
2023-08-02 17:44:59 +0200

    feat: add actions on users posts

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
@@ -9,6 +10,9 @@ class HomeController extends Controller
 {
     public function index()
     {
-        return Inertia::render('Home');
+        $allPosts = Post::with('user')->paginate(5);
+        return Inertia::render('Home', [
+            'posts' => $allPosts
+        ]);
     }
 }
--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -1,18 +1,74 @@
 <template>
   <Head title="Home" />
   <div>
-    <h2>Home Page</h2>
+    <h2>All Posts List</h2>
+    <div v-if="$page.props.posts.data.length">
+      <MDBRow>
+        <MDBCol
+          md="4"
+          sm="12"
+          v-for="post in $page.props.posts.data"
+          :key="post.id"
+        >
+          <card>
+            <template v-slot:header>
+              {{ post.title }}
+            </template>
+            <template v-slot:title>
+              {{ post.title }}
+            </template>
+            <template v-slot:body>
+              {{ post.description }}
+            </template>
+            <template v-slot:actions v-if="canViewActions(post)">
+              <div class="d-flex gap-4">
+                <Link class="btn btn-info">Edit</Link>
+                <Link
+                  class="btn btn-danger"
+                  method="delete"
+                  as="button"
+                  href="/posts/destroy"
+                  :data="{ postId: post.id }"
+                  >Delete</Link
+                >
+              </div>
+            </template>
+            <template v-slot:footer>
+              <MDBCardFooter class="text-muted"
+                >Post by: {{ post.user.names }}</MDBCardFooter
+              >
+            </template>
+          </card>
+        </MDBCol>
+      </MDBRow>
+    </div>
+    <span v-else>No Post Available</span>
   </div>
 </template>
 
 <script>
 import { Head } from "@inertiajs/vue3";
 import Layout from "../components/Layout.vue";
+import { MDBCol, MDBRow } from "mdb-vue-ui-kit";
 
 export default {
   layout: Layout,
   components: {
-    Head
-  }
+    Head,
+    MDBCol,
+    MDBRow,
+  },
+  props: {
+    posts: Object,
+  },
+
+  methods: {
+    canViewActions: function (post) {
+      return this.$page.props.auth.isAuth &&
+        this.$page.props.auth.user.id === post.user_id
+        ? true
+        : false;
+    },
+  },
 };
 </script>
